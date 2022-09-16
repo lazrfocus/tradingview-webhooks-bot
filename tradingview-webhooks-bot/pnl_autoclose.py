8aa01c4865fc742cd3ed0c81f09d2fe4fd20fa25
lazrfocus <52254582+lazrfocus@users.noreply.github.com>
2022-09-16 14:21:18 -0700

    update pnl_autoclose.py
    cleanup code

--- tradingview-webhooks-bot/pnl_autoclose.py
+++ tradingview-webhooks-bot/pnl_autoclose.py
@@ -17,24 +17,42 @@ percStop = -1
 while 1:
     totalPNL = 0 
     
+    #Get Long Positions
     try:
         positionsJSON = json.loads(subprocess.check_output(['ftx','positions','--sort','pnl','--output','json'],stderr=STDOUT,timeout=8))
-    except subprocess.TimeoutExpired:
-    #except subprocess.CalledProcessError as e:
-    #    print('read error')
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+        print("Error: ", e.returncode)
+        time.sleep(1)
         continue
-
+    except subprocess.TimeoutExpired as e:
+        print("Timeout: ", e)
+        time.sleep(1)
+        continue
+    except Exception as e:
+        print("Error: ", e)
+        time.sleep(1)
+        continue
+    
+    #Get Short Positions
     try:
-        positionsShortJSON = json.loads(subprocess.check_output(['ftx','positions','-a','Short','--sort','pnl','--output','json'],stderr=STDOUT,timeout=8))
-    except subprocess.TimeoutExpired:
+        positionsShortJSON = json.loads(subprocess.check_output(['ftx','positions','--sort','-pnl','--output','json'],stderr=STDOUT,timeout=8))
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+        print("Error: ", e.returncode)
+        time.sleep(1)
         continue
-    #except:
-    #except subprocess.CalledProcessError as e:
-    #    print('read error')
+    except subprocess.TimeoutExpired as e:
+        print("Timeout: ", e)
+        time.sleep(1)
         continue
-
+    except Exception as e:
+        print("Error: ", e)
+        time.sleep(1)
+        continue
+    
+    #Print system date for liveness check
     os.system('clear')
-
     date = datetime.now(tz=pytz.utc)
     date = date.astimezone(timezone('US/Pacific'))
     print(date.strftime(date_format),'\n')   
@@ -42,79 +60,61 @@ while 1:
     for p in positionsJSON:
         totalPNL = (totalPNL + p['pnl'])
         percPNL= 100*(p['markPrice'] - p['averageOpenPrice'])/p['averageOpenPrice']
-        if percPNL > percTP:
-            try:
-                json.loads(subprocess.check_output(['exlong',p['market'],'--output','json'],stderr=STDOUT,timeout=8))
-            except:
-                print('read error') 
-        elif percPNL < percStop:
-            try:
-                json.loads(subprocess.check_output(['exlong',p['market'],'--output','json'],stderr=STDOUT,timeout=8))
-            except:
-                print('read error')
-        elif percPNL < 0:
-            try:
-                print('\033[1m',p['market'],'\033[0m', ' | ', int(p['notionalSize']), ' | ', round(p['markPrice'],3), ' | ', round(p['averageOpenPrice'],3), ' | ', round(p['breakEvenPrice'],3), ' | ', round(p['estimatedLiquidationPrice'],3), ' | ', '\x1b[0;31;40m', round(p['pnl'],2),' | ',round(percPNL,1),'%','\x1b[0m')
-            except:
-                print('error')
-        else:
-            try:
-                print('\033[1m',p['market'],'\033[0m',' | ', int(p['notionalSize']), ' | ', round(p['markPrice'],3), ' | ', round(p['averageOpenPrice'],3), ' | ', round(p['breakEvenPrice'],3), ' | ', round(p['estimatedLiquidationPrice'],3), ' | ', '\x1b[0;32;40m', round(p['pnl'],2),' | ', round(percPNL,1),'%','\x1b[0m')
-            except:
-                print('error')
 
+        try:
+            if percPNL > percTP:
+                output=json.loads(subprocess.check_output(['exlong',p['market'],'--output','json'],stderr=STDOUT,timeout=8))
+            elif percPNL < percStop:
+                output=json.loads(subprocess.check_output(['exlong',p['market'],'--output','json'],stderr=STDOUT,timeout=8))
+            elif percPNL < 0:
+                print('\033[1m',p['market'],'\033[0m', ' | ', int(p['notionalSize']), ' | ', round(p['markPrice'],3), ' | ', round(p['averageOpenPrice'],3), ' | ', round(p['breakEvenPrice'],3), ' | ', round(p['estimatedLiquidationPrice'],3), ' | ', '\x1b[0;31;40m', round(p['pnl'],2),' | ',round(percPNL,1),'%','\x1b[0m')
+            else:
+                print('\033[1m',p['market'],'\033[0m',' | ', int(p['notionalSize']), ' | ', round(p['markPrice'],3), ' | ', round(p['averageOpenPrice'],3), ' | ', round(p['breakEvenPrice'],3), ' | ', round(p['estimatedLiquidationPrice'],3), ' | ', '\x1b[0;32;40m', round(p['pnl'],2),' | ', round(percPNL,1),'%','\x1b[0m')
+            print(output['message'])
+        except subprocess.CalledProcessError as e:
+            print(e.output)
+            print("Error: ", e.returncode)
+            time.sleep(1)
+            continue
+        except subprocess.TimeoutExpired as e:
+            print("Timeout: ", e)
+            time.sleep(1)
+            continue
+        except Exception as e:
+            print("Error: ", e)
+            time.sleep(1)
+            continue
+        
     print('\nShorts:')
 
     for p in positionsShortJSON:
-        percPNL=- 100*(p['markPrice'] - p['averageOpenPrice'])/p['averageOpenPrice']
         totalPNL = (totalPNL + p['pnl'])
-        if percPNL > percTP:
-            try:
-                json.loads(subprocess.check_output(['exshort',p['market'],'--output','json'],stderr=STDOUT,timeout=8))
-            except:
-                print('read error') 
-        elif percPNL < percStop:
-            try:
-                json.loads(subprocess.check_output(['exshort',p['market'],'--output','json'],stderr=STDOUT,timeout=8))
-            except:
-                print('read error')
-        elif percPNL < 0:
-            try:
+        percPNL=- 100*(p['markPrice'] - p['averageOpenPrice'])/p['averageOpenPrice']
+        
+        try:
+            if percPNL > percTP:
+                output=json.loads(subprocess.check_output(['exshort',p['market'],'--output','json'],stderr=STDOUT,timeout=8))
+            elif percPNL < percStop:
+                output=json.loads(subprocess.check_output(['exshort',p['market'],'--output','json'],stderr=STDOUT,timeout=8))
+            elif percPNL < 0:
                 print('\033[1m',p['market'],'\033[0m', ' | ', int(p['notionalSize']), ' | ', round(p['markPrice'],3), ' | ', round(p['averageOpenPrice'],3), ' | ', round(p['breakEvenPrice'],3), ' | ', round(p['estimatedLiquidationPrice'],3), ' | ', '\x1b[0;31;40m', round(p['pnl'],2),' | ',round(percPNL,1),'%','\x1b[0m')
-            except:
-                print('error')
-        else:
-            try:
+            else:
                 print('\033[1m',p['market'],'\033[0m',' | ', int(p['notionalSize']), ' | ', round(p['markPrice'],3), ' | ', round(p['averageOpenPrice'],3), ' | ', round(p['breakEvenPrice'],3), ' | ', round(p['estimatedLiquidationPrice'],3), ' | ', '\x1b[0;32;40m', round(p['pnl'],2),' | ', round(percPNL,1),'%','\x1b[0m')
-            except:
-                print('error')
-#    try:
-#        positionsShortJSON = json.loads(subprocess.check_output(['ftx','positions','-a','Short','--sort','pnl','--output','json'],stderr=STDOUT,timeout=8))
-#    except:
-#        print('read error') 
-#    
-#    os.system('clear')
-#
-#    date = datetime.now(tz=pytz.utc)
-#    date = date.astimezone(timezone('US/Pacific'))
-#    print(date.strftime(date_format),'\n')   
-#    
-#    for p in positionsJSON:
-#        totalPNL = totalPNL + p['pnl']
-#        if p['pnl']<0:
-#            print('\033[1m',p['market'],'\033[0m',' | ', int(p['notionalSize']), ' | ', round(p['markPrice'],3), ' | ', round(p['averageOpenPrice'],3), ' | ', round(p['breakEvenPrice'],3), ' | ', round(p['estimatedLiquidationPrice'],3), ' | ', '\x1b[0;31;40m', round(p['pnl'],3),'\x1b[0m')
-#        else:
-#            print('\033[1m',p['market'],'\033[0m',' | ', int(p['notionalSize']), ' | ', round(p['markPrice'],3), ' | ', round(p['averageOpenPrice'],3), ' | ', round(p['breakEvenPrice'],3), ' | ', round(p['estimatedLiquidationPrice'],3), ' | ', '\x1b[0;32;40m', round(p['pnl'],3),'\x1b[0m')
-#
-#    print('\nShorts:')
-#
-#    for p in positionsShortJSON:
-#        totalPNL = totalPNL + p['pnl']
-#        if p['pnl']<0:
-#            print('\033[1m',p['market'],'\033[0m', ' | ', int(p['notionalSize']), ' | ', round(p['markPrice'],3), ' | ', round(p['averageOpenPrice'],3), ' | ', round(p['breakEvenPrice'],3), ' | ', round(p['estimatedLiquidationPrice'],3), ' | ', '\x1b[0;31;40m', round(p['pnl'],3),'\x1b[0m')
-#        else:
-#            print('\033[1m',p['market'],'\033[0m',' | ', int(p['notionalSize']), ' | ', round(p['markPrice'],3), ' | ', round(p['averageOpenPrice'],3), ' | ', round(p['breakEvenPrice'],3), ' | ', round(p['estimatedLiquidationPrice'],3), ' | ', '\x1b[0;32;40m', round(p['pnl'],3),'\x1b[0m')
-#
+            print(output['message'])
+        except subprocess.CalledProcessError as e:
+            print(e.output)
+            print("Error: ", e.returncode)
+            time.sleep(1)
+            continue
+        except subprocess.TimeoutExpired as e:
+            print("Timeout: ", e)
+            time.sleep(1)
+            continue
+        except Exception as e:
+            print("Error: ", e)
+            time.sleep(1)
+            continue
+
     print('\nTotal: $',round(totalPNL,2))
 
     time.sleep(1)
