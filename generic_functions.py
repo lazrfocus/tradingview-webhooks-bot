3a290e343d83e9bcfd7c0e9a7601774075af044d
user <lazr@foc.us>
2022-11-01 15:18:21 -0700

    change pnl.py to use ccxt

---
+++ generic_functions.py
@@ -0,0 +1,16 @@
+import subprocess
+import datetime
+import re
+import pytz
+
+date_format='%m/%d/%Y %H:%M:%S %Z'
+def print_time():
+    date = datetime.datetime.now(tz=pytz.utc)
+    date = date.astimezone(pytz.timezone('US/Pacific'))
+    print(date.strftime(date_format))
+
+def print_ping():
+    ping_response = subprocess.Popen(["ping", "-c1", "ftx.com"], stdout=subprocess.PIPE).stdout.read()
+    print(re.search(r'time=(\d+)',ping_response.decode(), re.MULTILINE).group(1) +' ms')
+
+    
---
+++ pnl_old.py
@@ -0,0 +1,89 @@
+import json
+import subprocess
+from subprocess import STDOUT, check_output
+import os
+import time
+from datetime import datetime
+import pytz
+from pytz import timezone
+
+positionsJSON = {}
+positionsShortJSON = {}
+date_format='%m/%d/%Y %H:%M:%S %Z'
+
+while 1:
+    totalPNL = 0 
+    
+    try:
+        positionsJSON = json.loads(subprocess.check_output(['ftx','positions','--sort','pnl','--output','json'],stderr=STDOUT,timeout=8))
+    except subprocess.TimeoutExpired:
+    #except subprocess.CalledProcessError as e:
+    #    print('read error')
+        continue
+
+    try:
+        positionsShortJSON = json.loads(subprocess.check_output(['ftx','positions','-a','Short','--sort','pnl','--output','json'],stderr=STDOUT,timeout=8))
+    except subprocess.TimeoutExpired:
+        continue
+    #except:
+    #except subprocess.CalledProcessError as e:
+    #    print('read error')
+        continue
+
+    os.system('clear')
+
+    date = datetime.now(tz=pytz.utc)
+    date = date.astimezone(timezone('US/Pacific'))
+    print(date.strftime(date_format),'\n')   
+    
+    for p in positionsJSON:
+        totalPNL = (totalPNL + p['pnl'])
+        percPNL= 100*(p['markPrice'] - p['averageOpenPrice'])/p['averageOpenPrice']
+        if percPNL <=-5:
+            print('\033[1m',p['market'],'\033[0m', ' | ', int(p['notionalSize']), ' | ', round(p['markPrice'],3), ' | ', round(p['averageOpenPrice'],3), ' | ', round(p['breakEvenPrice'],3), ' | ', round(p['estimatedLiquidationPrice'],3), ' | ', '\x1b[0;41;1m', round(p['pnl'],2),' | ',round(percPNL,1),'%','\x1b[0m') 
+        elif percPNL > -5 and percPNL < 0: 
+            print('\033[1m',p['market'],'\033[0m',' | ', int(p['notionalSize']), ' | ', round(p['markPrice'],3), ' | ', round(p['averageOpenPrice'],3), ' | ', round(p['breakEvenPrice'],3), ' | ', round(p['estimatedLiquidationPrice'],3), ' | ', '\x1b[0;31;40m', round(p['pnl'],2),' | ',round(percPNL,1),'%','\x1b[0m')
+        else:
+            print('\033[1m',p['market'],'\033[0m',' | ', int(p['notionalSize']), ' | ', round(p['markPrice'],3), ' | ', round(p['averageOpenPrice'],3), ' | ', round(p['breakEvenPrice'],3), ' | ', round(p['estimatedLiquidationPrice'],3), ' | ', '\x1b[0;32;40m', round(p['pnl'],2),' | ', round(percPNL,1),'%','\x1b[0m')
+
+    print('\nShorts:')
+
+    for p in positionsShortJSON:
+        percPNL=- 100*(p['markPrice'] - p['averageOpenPrice'])/p['averageOpenPrice']
+        totalPNL = (totalPNL + p['pnl'])
+        if percPNL <= -5:
+            print('\033[1m',p['market'],'\033[0m', ' | ', int(p['notionalSize']), ' | ', round(p['markPrice'],3), ' | ', round(p['averageOpenPrice'],3), ' | ', round(p['breakEvenPrice'],3), ' | ', round(p['estimatedLiquidationPrice'],3), ' | ', '\x1b[0;41;1m', round(p['pnl'],2),' | ',round(percPNL,1),'%','\x1b[0m')
+        elif percPNL > -5 and percPNL < 0:
+            print('\033[1m',p['market'],'\033[0m', ' | ', int(p['notionalSize']), ' | ', round(p['markPrice'],3), ' | ', round(p['averageOpenPrice'],3), ' | ', round(p['breakEvenPrice'],3), ' | ', round(p['estimatedLiquidationPrice'],3), ' | ', '\x1b[0;31;40m', round(p['pnl'],2),' | ',round(percPNL,1),'%','\x1b[0m')
+        else:
+            print('\033[1m',p['market'],'\033[0m',' | ', int(p['notionalSize']), ' | ', round(p['markPrice'],3), ' | ', round(p['averageOpenPrice'],3), ' | ', round(p['breakEvenPrice'],3), ' | ', round(p['estimatedLiquidationPrice'],3), ' | ', '\x1b[0;32;40m', round(p['pnl'],2),' | ',round(percPNL,1),'%','\x1b[0m')
+#    try:
+#        positionsShortJSON = json.loads(subprocess.check_output(['ftx','positions','-a','Short','--sort','pnl','--output','json'],stderr=STDOUT,timeout=8))
+#    except:
+#        print('read error') 
+#    
+#    os.system('clear')
+#
+#    date = datetime.now(tz=pytz.utc)
+#    date = date.astimezone(timezone('US/Pacific'))
+#    print(date.strftime(date_format),'\n')   
+#    
+#    for p in positionsJSON:
+#        totalPNL = totalPNL + p['pnl']
+#        if p['pnl']<0:
+#            print('\033[1m',p['market'],'\033[0m',' | ', int(p['notionalSize']), ' | ', round(p['markPrice'],3), ' | ', round(p['averageOpenPrice'],3), ' | ', round(p['breakEvenPrice'],3), ' | ', round(p['estimatedLiquidationPrice'],3), ' | ', '\x1b[0;31;40m', round(p['pnl'],3),'\x1b[0m')
+#        else:
+#            print('\033[1m',p['market'],'\033[0m',' | ', int(p['notionalSize']), ' | ', round(p['markPrice'],3), ' | ', round(p['averageOpenPrice'],3), ' | ', round(p['breakEvenPrice'],3), ' | ', round(p['estimatedLiquidationPrice'],3), ' | ', '\x1b[0;32;40m', round(p['pnl'],3),'\x1b[0m')
+#
+#    print('\nShorts:')
+#
+#    for p in positionsShortJSON:
+#        totalPNL = totalPNL + p['pnl']
+#        if p['pnl']<0:
+#            print('\033[1m',p['market'],'\033[0m', ' | ', int(p['notionalSize']), ' | ', round(p['markPrice'],3), ' | ', round(p['averageOpenPrice'],3), ' | ', round(p['breakEvenPrice'],3), ' | ', round(p['estimatedLiquidationPrice'],3), ' | ', '\x1b[0;31;40m', round(p['pnl'],3),'\x1b[0m')
+#        else:
+#            print('\033[1m',p['market'],'\033[0m',' | ', int(p['notionalSize']), ' | ', round(p['markPrice'],3), ' | ', round(p['averageOpenPrice'],3), ' | ', round(p['breakEvenPrice'],3), ' | ', round(p['estimatedLiquidationPrice'],3), ' | ', '\x1b[0;32;40m', round(p['pnl'],3),'\x1b[0m')
+#
+    print('\nTotal: $',round(totalPNL,2))
+
+    time.sleep(1)
