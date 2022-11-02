7f4e9308bc5d3f3c8e98a2f16fefdac194f7d6e6
user <lazr@foc.us>
2022-11-01 21:08:49 -0700

    make ascii chart a tick chart w/ websockets

--- chart.py
+++ chart.py
@@ -3,93 +3,116 @@
 import os
 import sys
 import subprocess
+from ftx import ThreadedWebsocketManager
+import api_keys
 import asyncio
-import numpy as np
 
 from asciichartpy import plot
 
-# -----------------------------------------------------------------------------
-
-this_folder = os.path.dirname(os.path.abspath(__file__))
-root_folder = os.path.dirname(os.path.dirname(this_folder))
-sys.path.append(root_folder + '/python')
-sys.path.append(this_folder)
-
-# -----------------------------------------------------------------------------
-
-import ccxt  # noqa: E402
-
-# -----------------------------------------------------------------------------
-
-ftx = ccxt.ftx()
-#coinbasepro = ccxt.coinbasepro()
+API = api_keys.FTX_KEY
+SECRET = api_keys.FTX_SECRET
 
 symbol = 'DOGE-PERP'
-
-# each ohlcv candle is a list of [ timestamp, open, high, low, close, volume ]
-index = 4  # use close price from each ohlcv candle
-
-ohlcv = None
-series = None
-
 priceArray = []
-
-root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-sys.path.append(root + '/python')
-
-import ccxt.async_support as ccxt  # noqa: E402
+bidArray = []
+askArray = []
 
 def print_chart():
-    # print the chart
-    # global ohlcv
-    # global series
     global priceArray
-    
+    global askArray
+    global bidArray
     subprocess.call('clear', shell=True)            
-    # print(plot(series, { 'height': 10 }))
-    print("\n" + plot(priceArray[-200:], {'height': 40, 'format':'{:8.4f}'}))
+    print("\n" + plot([priceArray, askArray, bidArray], {'height': 8, 'format':'{:8.4f}'}),)
+    print("\n" + symbol + "= $" + str(priceArray[-1]))  # print last closing price            
+    print("\n", round((askArray[-1]-bidArray[-1])/((askArray[-1]+bidArray[-1])/2)*100,4),"%" + " spread")
 
-    # last = ohlcv[len(ohlcv) - 1][index]  # last closing price
-    print("\n" + symbol + "= $" + str(priceArray[-1]) + "\n")  # print last closing price            
-    # return last
-
-async def main(symbol):
-    # you can set enableRateLimit = True to enable the built-in rate limiter
-    # this way you request rate will never hit the limit of an exchange
-    # the library will throttle your requests to avoid that
+def on_read(payload):
     global priceArray
-    exchange = ccxt.ftx()
-    while True:
-        # print('--------------------------------------------------------------')
-        # print(exchange.iso8601(exchange.milliseconds()), 'fetching', symbol, 'ticker from', exchange.name)
-        # this can be any call instead of fetch_ticker, really
-        try:
-            ticker = await exchange.fetch_ticker(symbol)
-            # print(exchange.iso8601(exchange.milliseconds()), 'fetched', symbol, 'ticker from', exchange.name)
-            #print(ticker)
-            #print(ticker['info']['last'])
-            priceArray.append(float(ticker['info']['last']))
-            if len(priceArray) > 200:
+    global askArray
+    global bidArray
+    # print(payload.keys())
+    if payload['channel'] == 'ticker':
+        if payload['type'] == 'update':
+            priceArray.append(payload['data']['last'])
+            if len(priceArray)>140:
                 priceArray.pop(0)
-            # print(priceArray)
-            print_chart()
+            askArray.append(payload['data']['ask'])
+            if len(askArray)>140:
+                askArray.pop(0)
+            bidArray.append(payload['data']['bid'])
+            if len(bidArray)>140:
+                bidArray.pop(0)
+            print_chart()    
 
-        except ccxt.RequestTimeout as e:
-            print('[' + type(e).__name__ + ']')
-            print(str(e)[0:200])
-            # will retry
-        except ccxt.DDoSProtection as e:
-            print('[' + type(e).__name__ + ']')
-            print(str(e.args)[0:200])
-            # will retry
-        except ccxt.ExchangeNotAvailable as e:
-            print('[' + type(e).__name__ + ']')
-            print(str(e.args)[0:200])
-            # will retry
-        except ccxt.ExchangeError as e:
-            print('[' + type(e).__name__ + ']')
-            print(str(e)[0:200])
-            break  # won't retry
+if __name__ == '__main__':
+    
+    wsm = ThreadedWebsocketManager(API, SECRET)
+    wsm.start()
+    name = 'market_connection'
+    wsm.start_socket(on_read, socket_name=name)
+    wsm.subscribe(name, channel="ticker", op="subscribe", market=symbol)
+
+    # try:
+    #     while True:
+    #         # asyncio.new_event_loop().run_until_complete(get_ohlc(ftx, 'DOGE-PERP', '1m'))
+
+    #         # print_chart()
+    #         # asyncio.run(fetch_ticker('DOGE-PERP'))
+    #         exchange = ccxt.ftx()
+    #         orderbook = await exchange.
+    #         now = exchange.milliseconds()
+    #         print(exchange.iso8601(now), symbol, orderbook['asks'][0], orderbook['bids'][0])
+    #     # except Exception as e:
+    #     #     print(str(e))
+    #     #     #raise e  # uncomment to break all loops in case of an error in any one of them
+    #     #     break  # you can also break just this one loop if it fails
+            
+            
+
+    # except KeyboardInterrupt:
+    #     print('Interrupted')
+    #     os._exit(0)
+        
+
+
+
+# async def fetch_ticker(symbol):
+#     # you can set enableRateLimit = True to enable the built-in rate limiter
+#     # this way you request rate will never hit the limit of an exchange
+#     # the library will throttle your requests to avoid that
+#     global priceArray
+#     exchange = ccxt.ftx()
+#     while True:
+#         # print('--------------------------------------------------------------')
+#         # print(exchange.iso8601(exchange.milliseconds()), 'fetching', symbol, 'ticker from', exchange.name)
+#         # this can be any call instead of fetch_ticker, really
+#         try:
+#             ticker = await exchange.fetch_ticker(symbol)
+#             # print(exchange.iso8601(exchange.milliseconds()), 'fetched', symbol, 'ticker from', exchange.name)
+#             #print(ticker)
+#             #print(ticker['info']['last'])
+#             priceArray.append(float(ticker['info']['last']))
+#             if len(priceArray) > 200:
+#                 priceArray.pop(0)
+#             # print(priceArray)
+#             print_chart()
+
+#         except ccxt.RequestTimeout as e:
+#             print('[' + type(e).__name__ + ']')
+#             print(str(e)[0:200])
+#             # will retry
+#         except ccxt.DDoSProtection as e:
+#             print('[' + type(e).__name__ + ']')
+#             print(str(e.args)[0:200])
+#             # will retry
+#         except ccxt.ExchangeNotAvailable as e:
+#             print('[' + type(e).__name__ + ']')
+#             print(str(e.args)[0:200])
+#             # will retry
+#         except ccxt.ExchangeError as e:
+#             print('[' + type(e).__name__ + ']')
+#             print(str(e)[0:200])
+#             break  # won't retry
 
 # async def get_ohlc(exchange, symbol, timeframe):
 #     global ohlcv
@@ -106,18 +129,3 @@ async def main(symbol):
 #     await ftx.close()  # don't forget to close it when you're done
 #     # get the ohlCv (closing price, index == 4)
 #     series = [x[index] for x in ohlcv]
-
-if __name__ == '__main__':
-    
-    try:
-        while True:
-            # asyncio.new_event_loop().run_until_complete(get_ohlc(ftx, 'DOGE-PERP', '1m'))
-
-            # print_chart()
-            asyncio.run(main('DOGE-PERP'))
-            
-    except KeyboardInterrupt:
-        print('Interrupted')
-        os._exit(0)
-        
-
