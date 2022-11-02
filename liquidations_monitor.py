beabaae1baff10c2290cfbe30a839c66c55d1cd0
user <lazr@foc.us>
2022-11-01 23:17:39 -0700

    add liquidations monitor

---
+++ liquidations_monitor.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import subprocess
+from ftx import ThreadedWebsocketManager
+import api_keys
+import subprocess
+from asciichartpy import plot
+import generic_functions as func
+#from termgraph import CandleStickGraph
+
+API = api_keys.FTX_KEY
+SECRET = api_keys.FTX_SECRET
+
+symbol = 'DOGE-PERP'
+
+def on_read(payload):
+    # print(payload['channel'])
+    if payload['channel'] == 'trades':
+        if payload['type'] == 'update':
+            for i in payload['data']:
+                if i['liquidation'] == 'True':
+                    print("LIQUIDATION", payload['data']['side'], payload['data']['size'], payload['data']['price'], payload['data']['time'],'\n')
+                else:
+                    pass
+
+def subscribe_trades(symbol):
+    wsm = ThreadedWebsocketManager(API, SECRET)
+    wsm.start()
+    name = 'market_connection'
+    wsm.start_socket(on_read, socket_name=name)
+    wsm.subscribe(name, channel="trades", op="subscribe", market=symbol)
+
+if __name__ == '__main__':
+    subprocess.call('clear', shell=True)            
+    
+    try:
+        subscribe_trades(symbol)
+    
+    except KeyboardInterrupt:
+        print('Interrupted')
+        os._exit(0)
+        
