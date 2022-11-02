c721be605275f9f68eb5941f0fba2133ea619669
user <lazr@foc.us>
2022-11-02 01:57:18 -0700

    add orderbook firstbid first ask monitoring test

--- monitor_orderbook.py
+++ monitor_orderbook.py
@@ -3,41 +3,68 @@ from ftx import ThreadedWebsocketManager
 import api_keys
 import subprocess
 import generic_functions as func
+from decimal import Decimal
+
+import requests
+from order_book import OrderBook
 
 API = api_keys.FTX_KEY
 SECRET = api_keys.FTX_SECRET
 
 symbol = 'DOGE-PERP'
 
-orderbook = {}
+# orderbook = {}
+def test_orderbook_init():
+    global ob
+    ob = OrderBook(max_depth=10)
+    #ob.bids = {{val: val for val in range(20)}}
+    #ob.asks = {val: val for val in range(10, 30)}
+    ob.bids = {}
+    ob.asks = {}
+    # assert len(ob) == 20
 
 def on_OBupdate(payload):
-    # print(payload)
-    # print(payload['channel'])
-    # setactive=0
+    global ob
     if payload['channel'] == 'orderbook':
         if payload['type'] == 'update':
             for i in payload['data']['bids']:
                 if i[1] == 0:
-                    print(i[0],"bid removed")
+                    # print(i[0],"bid removed")
+                    try:
+                        del ob.bids[Decimal(i[0])]
+                    except:
+                        pass
                 else:
-                    print(i[0],"bid added")
-                    orderbook[str(i[0])]=float(orderbook[i[0]])+float(i[1])
+                    # print(i[0],"bid added")
+                    ob.bids[Decimal(i[0])] = Decimal(i[1])
+
+
             for i in payload['data']['asks']:
                 if i[1] == 0:
-                    print(i[0],"ask removed")
+                    # print(i[0],"ask removed")
+                    try:
+                        del ob.asks[Decimal(i[0])]
+                    except:
+                        pass
                 else:
-                    print(i[0],"ask added")
+                    # print(i[0],"ask added")
+                    ob.asks[Decimal(i[0])] = Decimal(i[1])
 
-    #             # print(bool(i['liquidation']))
-    #             if bool(i['liquidation']):
-    #                 print(i['time'], "     |  ", i['size'], "   ", i['price'])
-    #                 rollingliquidations = float(i['size'])+rollingliquidations
-    #                 setactive=1
-    #             else:
-    #                 if setactive:
-    #                     print("Liquidations since start: ", round(rollingliquidations),2)
-    #                     setactive=0
+    # convert orderbook to dict
+    def convert(x):
+        if isinstance(x, Decimal):
+            return float(x)
+        if isinstance(x, dict):
+            return {k: float(v) for k, v in x.items()}
+    ob_dict = ob.to_dict(to_type=convert)
+    try:
+        topbid=float(list(ob_dict['bid'].keys())[0])
+        topask=float(list(ob_dict['ask'].keys())[0])
+        print("First Bid   ", topbid)
+        print("First Ask   ", topask)
+
+    except:
+        pass
 
 def subscribe_book(symbol):
     wsm = ThreadedWebsocketManager(API, SECRET)
@@ -50,7 +77,9 @@ if __name__ == '__main__':
     subprocess.call('clear', shell=True)            
     
     try:
+        test_orderbook_init() 
         subscribe_book(symbol)
+         
         
     except KeyboardInterrupt:
         print('Interrupted')
